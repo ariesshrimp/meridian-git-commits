db1ae2e1b2a02f88568e14f19f5bb9230152454f
joefraley <jose.fraley@gmail.com>
2017-03-20 02:18:02 -0700

    feat(aarstarst): artarstrsat
    @joefraley
    
    BREAKING CHANGE: aarsrat | artarts | arstarstat | arstarsttr

--- src/post-version.js
+++ src/post-version.js
@@ -25,32 +25,36 @@ const updateRepo = async () => {
   });
   const repo = await gh.getRepo(options.USER_NAME, options.repo.name);
 
-  console.log('\nCheckout a new release branch...\n');
+  // console.log('\nCheckout a new release branch...\n');
   shell.exec(`git checkout -b release-v${packageJson.version}`);
   shell.exec(
     `git commit --amend -m  "chore(release): v${packageJson.version} [skip ci]"`
   );
-
-  console.log('\nSend new release branch up to remote...\n');
-  shell.exec(`git push origin release-v${packageJson.version}`);
-
-  console.log('\nCreate a pull request to master with new version...\n');
-  const {data: {number}} = await repo.createPullRequest({
-    title: `chore(release): v${packageJson.version}`,
-    body: releaseNotes,
-    base: 'master',
-    head: `release-v${packageJson.version}`,
-  });
-
-  console.log('\nAutomatically merge request...\n');
-  const merge = await repo.mergePullRequest(number, {
-    merge_method: 'squash',
-  });
-
-  console.log('\nDelete remote release branch\n');
-  const deadBranch = await repo.deleteRef(
-    `heads/release-v${packageJson.version}`
+  shell.exec(
+    `git checkout master && git rebase release-v${packageJson.version}`
   );
+  shell.exec(`git push -f origin master`);
+
+  // console.log('\nSend new release branch up to remote...\n');
+  // shell.exec(`git push origin release-v${packageJson.version}`);
+
+  // console.log('\nCreate a pull request to master with new version...\n');
+  // const {data: {number}} = await repo.createPullRequest({
+  //   title: `chore(release): v${packageJson.version}`,
+  //   body: releaseNotes,
+  //   base: 'master',
+  //   head: `release-v${packageJson.version}`,
+  // });
+
+  // console.log('\nAutomatically merge request...\n');
+  // const merge = await repo.mergePullRequest(number, {
+  //   merge_method: 'squash',
+  // });
+
+  // console.log('\nDelete remote release branch\n');
+  // const deadBranch = await repo.deleteRef(
+  //   `heads/release-v${packageJson.version}`
+  // );
 
   console.log('\nAdd release notes...\n');
   const release = await repo.createRelease({
