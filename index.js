b144a0abecbbee92f7f507e287b32c524da16413
joefraley <jose.fraley@gmail.com>
2017-03-19 17:24:29 -0700

    fix(releases): Automate release cycle.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 bootstrap({
   cliPath: path.join(__dirname, 'node_modules/commitizen'),
   config: {
-    path: './node_modules/cz-conventional-changelog-lint',
+    path: './node_modules/cz-conventional-changelog',
   },
 });
