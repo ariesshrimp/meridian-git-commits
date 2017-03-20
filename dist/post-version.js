54eab90239543ebf7bf4e2dc3ad4cc65b7cbb5db
joefraley <jose.fraley@gmail.com>
2017-03-20 02:18:02 -0700

    chore(release): v1.0.1 [skip ci]

--- dist/post-version.js
+++ dist/post-version.js
@@ -46,8 +46,7 @@ var releaseNotes = _ramda2.default.pipe(_fs2.default.readFileSync, _ramda2.defau
 
 var updateRepo = function () {
   var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee() {
-    var gh, repo, _ref2, number, merge, deadBranch, release;
-
+    var gh, repo, release;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -62,57 +61,48 @@ var updateRepo = function () {
             repo = _context.sent;
 
 
-            console.log('\nCheckout a new release branch...\n');
+            // console.log('\nCheckout a new release branch...\n');
             _shelljs2.default.exec('git checkout -b release-v' + _package2.default.version);
             _shelljs2.default.exec('git commit --amend -m  "chore(release): v' + _package2.default.version + ' [skip ci]"');
+            _shelljs2.default.exec('git rebase master');
+            _shelljs2.default.exec('git push -f origin master');
 
-            console.log('\nSend new release branch up to remote...\n');
-            _shelljs2.default.exec('git push origin release-v' + _package2.default.version);
+            // console.log('\nSend new release branch up to remote...\n');
+            // shell.exec(`git push origin release-v${packageJson.version}`);
 
-            console.log('\nCreate a pull request to master with new version...\n');
-            _context.next = 12;
-            return repo.createPullRequest({
-              title: 'chore(release): v' + _package2.default.version,
-              body: releaseNotes,
-              base: 'master',
-              head: 'release-v' + _package2.default.version
-            });
+            // console.log('\nCreate a pull request to master with new version...\n');
+            // const {data: {number}} = await repo.createPullRequest({
+            //   title: `chore(release): v${packageJson.version}`,
+            //   body: releaseNotes,
+            //   base: 'master',
+            //   head: `release-v${packageJson.version}`,
+            // });
 
-          case 12:
-            _ref2 = _context.sent;
-            number = _ref2.data.number;
+            // console.log('\nAutomatically merge request...\n');
+            // const merge = await repo.mergePullRequest(number, {
+            //   merge_method: 'squash',
+            // });
 
+            // console.log('\nDelete remote release branch\n');
+            // const deadBranch = await repo.deleteRef(
+            //   `heads/release-v${packageJson.version}`
+            // );
 
-            console.log('\nAutomatically merge request...\n');
-            _context.next = 17;
-            return repo.mergePullRequest(number, {
-              merge_method: 'squash'
-            });
-
-          case 17:
-            merge = _context.sent;
-
-
-            console.log('\nDelete remote release branch\n');
-            _context.next = 21;
-            return repo.deleteRef('heads/release-v' + _package2.default.version);
-
-          case 21:
-            deadBranch = _context.sent;
-
-
+            //
+            //
+            //
             console.log('\nAdd release notes...\n');
-            _context.next = 25;
+            _context.next = 11;
             return repo.createRelease({
               tag_name: 'v' + _package2.default.version,
               name: 'v' + _package2.default.version,
               body: releaseNotes
             });
 
-          case 25:
+          case 11:
             release = _context.sent;
 
-          case 26:
+          case 12:
           case 'end':
             return _context.stop();
         }
