ebc50a622976c690deedb355938000730be46089
joefraley <jose.fraley@gmail.com>
2017-03-21 14:42:52 -0700

    feat(asrtarst): rastrastpfwqgqwfpwqfpwqf
    pqwfpwqfpqwfpwqfp
    
    BREAKING CHANGE: qfwpwqfpwqfpqwfp

--- dist/post-version.js
+++ dist/post-version.js
@@ -23,18 +23,23 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var _console = console,
     log = _console.log;
 exports.default = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee() {
+  var currentBranch;
   return _regenerator2.default.wrap(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          (0, _shelljs.exec)('git push --force --follow-tags origin `git branch | grep \* | cut -d " " -f2-`:master');
+          currentBranch = (0, _shelljs.exec)('git branch | grep \* | cut -d " " -f2-').toString().trim();
           _context.next = 3;
-          return (0, _conventionalGithubReleaser2.default)({ type: 'oauth', token: process.env.GH_TOKEN }, { preset: 'angular' }, log);
+          return (0, _shelljs.exec)('git push --force --follow-tags origin ' + currentBranch + ':master');
 
         case 3:
+          _context.next = 5;
+          return (0, _conventionalGithubReleaser2.default)({ type: 'oauth', token: process.env.GH_TOKEN }, { preset: 'angular' }, log);
+
+        case 5:
           return _context.abrupt('return', _context.sent);
 
-        case 4:
+        case 6:
         case 'end':
           return _context.stop();
       }
--- src/post-version.js
+++ src/post-version.js
@@ -3,9 +3,10 @@ import {exec} from 'shelljs';
 const {log} = console;
 
 export default async () => {
-  exec(
-    'git push --force --follow-tags origin `git branch | grep \* | cut -d " " -f2-`:master'
-  );
+  const currentBranch = exec('git branch | grep \* | cut -d " " -f2-')
+    .toString()
+    .trim();
+  await exec(`git push --force --follow-tags origin ${currentBranch}:master`);
   return await conventionalGithubReleaser(
     {type: 'oauth', token: process.env.GH_TOKEN},
     {preset: 'angular'},
